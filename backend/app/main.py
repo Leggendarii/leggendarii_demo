48c867dd0eb562bfa4a50254ce12ca7f426b000e
Nicolae Darii <nidar@dtu.dk>
2026-07-27 01:07:54 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .models import SCRRequest
+from .calculator import calculate_scr
+
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def root():
     return {"message": "Backend is running"}
+
+
+@app.post("/calculate")
+def calculate(request: SCRRequest):
+
+    result = calculate_scr(
+        request.voltage,
+        request.power,
+        request.xr,
+        request.frequency
+    )
+
+    return result
