f22e905069bb44b89857476095077b2a1f35e9da
Nicolae Darii <nidar@dtu.dk>
2026-07-27 01:08:55 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,36 @@
-function calculate() {
+async function calculate() {
 
-    console.log("Calculate");
+    const voltage = document.getElementById("voltage").value;
+    const power = document.getElementById("power").value;
+    const xr = document.getElementById("xr").value;
+    const frequency = document.getElementById("frequency").value;
+
+
+    const response = await fetch(
+        "http://127.0.0.1:8000/calculate",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                voltage: Number(voltage),
+                power: Number(power),
+                xr: Number(xr),
+                frequency: Number(frequency)
+            })
+        }
+    );
+
+
+    const data = await response.json();
+
+
+    document.getElementById("scr").innerHTML = data.scr;
+    document.getElementById("z").innerHTML = data.z;
+    document.getElementById("r").innerHTML = data.r;
+    document.getElementById("x").innerHTML = data.x;
 
 }
