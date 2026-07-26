90848ce0e1d2df9468ee5a97a1b239f359bbcedd
Nicolae Darii <nidar@dtu.dk>
2026-07-27 01:24:52 +0200

    Update calculator.py

--- backend/app/calculator.py
+++ backend/app/calculator.py
@@ -1,16 +1,9 @@
 def calculate_scr(voltage, power, xr, frequency):
 
-    scr = 5.0
+    # qui metteremo la formula vera
 
-    z = 0.1
-
-    r = z / ((1 + xr**2) ** 0.5)
-
-    x = r * xr
+    scr = 5
 
     return {
-        "scr": scr,
-        "z": z,
-        "r": r,
-        "x": x
+        "SCR": scr
     }
