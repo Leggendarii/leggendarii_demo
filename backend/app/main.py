fb537427cc9d09485bcc2a19309d96e3716a79ea
Nicolae Darii <nidar@dtu.dk>
2026-07-27 13:16:33 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,10 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://leggendarii.github.io"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
