bd6b8ffe37fa75b7f0884c36cb2b2d73d2b63908
Nicolae Darii <nidar@dtu.dk>
2026-07-27 12:51:02 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-from calculator import calculate_scr
+from app.calculator import calculate_scr
 
 
 app = FastAPI()
